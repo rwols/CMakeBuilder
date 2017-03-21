f803bbf2f7789c798240049566c8fe82c637e143
Raoul Wols <raoulwols@gmail.com>
2017-03-21 17:06:54 +0100

    Fix generators on osx

--- Generators/osx/Ninja.py
+++ Generators/osx/Ninja.py
@@ -1,20 +1,38 @@
 from .. import CMakeGenerator
 import subprocess
+import sublime
 
 class Ninja(CMakeGenerator):
 
     def __repr__(self):
         return 'Ninja'
 
-    def syntax(self):
+    def file_regex(self):
         return r'(.+[^:]):(\d+):(\d+): (?:fatal )?((?:error|warning): .+)$'
 
-    def file_regex(self):
+    def syntax(self):
         return 'Packages/CMakeBuilder/Syntax/Ninja.sublime-syntax'
 
     def variants(self):
-        lines = subprocess.check_output('cmake --build . --target help', cwd=self.build_folder).decode('utf-8').splitlines()
-        variants = []
+        env = None
+        if self.window.active_view():
+            env = self.window.active_view().settings().get('build_env')
+            
+        shell_cmd = 'cmake --build . --target help'
+        proc = subprocess.Popen(
+            ['/bin/bash', '-l', '-c', shell_cmd],
+            env=env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=False,
+            cwd=self.build_folder)
+        outs, errs = proc.communicate()
+        errs = errs.decode('utf-8')
+        if errs:
+            sublime.error_message(errs)
+            return
+        lines = outs.decode('utf-8').splitlines()
+        
         EXCLUDES = [
             'are some of the valid targets for this Makefile:',
             'All primary targets available:', 
@@ -27,29 +45,32 @@ class Ninja(CMakeGenerator):
             '.i',
             '.s']
 
-        LIB_EXTENSIONS = [
-            '.so',
-            '.dll',
-            '.dylib',
-            '.a']
+        variants = []
         for target in lines:
             try:
                 if any(exclude in target for exclude in EXCLUDES): 
                     continue
+                print(target)
                 target = target.rpartition(':')[0]
                 name = target
-                for ext in LIB_EXTENSIONS:
-                    if name.endswith(ext):
-                        name = name[:-len(ext)]
-                        break
+                # for ext in LIB_EXTENSIONS:
+                #     if name.endswith(ext):
+                #         name = name[:-len(ext)]
+                #         break
                 if (self.filter_targets and 
                     not any(f in name for f in self.filter_targets)):
                     continue
                 shell_cmd = 'cmake --build . --target {}'.format(target)
-                self.variants.append({'name': name, 'shell_cmd': shell_cmd})
+                variants.append({'name': name, 'shell_cmd': shell_cmd})
             except Exception as e:
                 print(e)
                 sublime.error_message(str(e))
         return variants
+
+    def on_data(self, proc, data):
+        pass
+
+    def on_finished(self, proc):
+        pass
 
         
