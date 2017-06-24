34fae01f255a147255320cd5925973f56cb02479
Raoul Wols <raoulwols@gmail.com>
2017-06-25 01:07:00 +0200

    Prepare for version 1.0

--- __init__.py
+++ __init__.py
@@ -1,10 +1,5 @@
 __version__ = "1.0.0"
 __version_info__ = (1,0,0)
 
-import sublime
-import sublime_plugin
-import subprocess
-import os
-import json
 from CMakeBuilder.commands import *
 from CMakeBuilder.event_listeners import *
