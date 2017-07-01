449cfdebc2a477ba8ce1573885d2f67b9778f8e6
Raoul Wols <raoulwols@gmail.com>
2017-07-01 17:41:15 +0200

    Fix bug where project would configure upon opening project file

--- event_listeners/configure_on_save.py
+++ event_listeners/configure_on_save.py
@@ -9,16 +9,6 @@ def _configure(window):
     window.run_command("cmake_configure")
 
 class ConfigureOnSave(sublime_plugin.EventListener):
-    
-    def on_activated(self, view):
-        if view and view.file_name() and view.file_name().endswith(".sublime-project"):
-            settings = view.settings()
-            if settings.get("cmake") and get_setting(view, "configure_on_save", False):
-                settings.add_on_change("cmake", functools.partial(_configure, view.window()))
-
-    def on_deactivated(self, view):
-        if view and view.file_name() and view.file_name().endswith(".sublime-project"):
-            view.settings().clear_on_change("cmake")
 
     def on_post_save(self, view):
         if not view:
@@ -28,5 +18,7 @@ class ConfigureOnSave(sublime_plugin.EventListener):
         name = view.file_name()
         if not name:
             return
-        if name.endswith("CMakeLists.txt") or name.endswith("CMakeCache.txt"):
+        if (name.endswith("CMakeLists.txt") or 
+            name.endswith("CMakeCache.txt") or 
+            name.endswith(".sublime-project")):
             _configure(view.window())
