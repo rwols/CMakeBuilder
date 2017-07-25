054649c2f975e92470950b97201bd7ea13211a1f
Raoul Wols <raoulwols@gmail.com>
2017-07-25 22:16:51 +0200

    Improvements to target handling

--- commands/build.py
+++ commands/build.py
@@ -1,6 +1,8 @@
 import sublime
 import Default.exec
+import os
 from .command import CmakeCommand, ServerManager
+
 
 class CmakeExecCommand(Default.exec.ExecCommand):
 
@@ -11,32 +13,53 @@ class CmakeExecCommand(Default.exec.ExecCommand):
             return
         self.server.is_building = True
         super().run(**kwargs)
-    
+
     def on_finished(self, proc):
         super().on_finished(proc)
         self.server.is_building = False
 
 
 class CmakeBuildCommand(CmakeCommand):
-    
+
     def run(self, select=False):
         if not self.is_enabled():
             sublime.error_message("Cannot build a CMake target!")
             return
-        active_target = self.window.project_data().get("settings", {}).get("active_target", None)
+        path = os.path.join(self.server.cmake.build_folder,
+                            "CMakeFiles",
+                            "CMakeBuilder",
+                            "active_target.txt")
+        if os.path.exists(path):
+            with open(path, "r") as f:
+                active_target = int(f.read())
+        else:
+            active_target = None
         if select or active_target is None:
             if not self.server.targets:
-                sublime.error_message("No targets found. Did you configure the project?")
-            self.items = [ [t.name, t.type, t.directory] for t in self.server.targets ]
+                sublime.error_message(
+                    "No targets found. Did you configure the project?")
+            self.items = [
+                [t.name, t.type, t.directory] for t in self.server.targets]
             self.window.show_quick_panel(self.items, self._on_done)
         else:
             self._on_done(active_target)
 
     def _on_done(self, index):
-        self.window.run_command("cmake_set_target", {"index": index})
-        if index == -1:
+        if isinstance(index, str):
+            self.window.run_command("cmake_set_target", {"name": index})
+            target = None
+            for t in self.server.targets:
+                if t.name == index:
+                    target = t
+                    break
+        elif isinstance(index, int):
+            if index == -1:
+                return
+            target = self.server.targets[index]
+            self.window.run_command("cmake_set_target", {"index": index})
+        else:
+            sublime.error_message("Unknown type: " + type(index))
             return
-        target = self.server.targets[index]
         if target.type == "RUN":
             if sublime.platform() in ("linux", "osx"):
                 prefix = "./"
@@ -53,7 +76,8 @@ class CmakeBuildCommand(CmakeCommand):
             self.window.run_command(
                 "cmake_exec", {
                     "window_id": self.window.id(),
-                    "cmd": self.cmake.cmd(None if target.type == "ALL" else target),
+                    "cmd": self.cmake.cmd(
+                        None if target.type == "ALL" else target),
                     "file_regex": self.cmake.file_regex(),
                     "syntax": self.cmake.syntax(),
                     "working_dir": self.cmake.build_folder
