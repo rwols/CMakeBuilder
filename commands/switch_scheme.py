b149cde62a3eda3ba851f56ab0a7ae8b1daf5d48
Raoul Wols <raoulwols@gmail.com>
2017-08-12 16:43:17 +0200

    Rename build system, and fix syntax mistake

--- commands/switch_scheme.py
+++ commands/switch_scheme.py
@@ -4,7 +4,7 @@ from .command import CmakeCommand, ServerManager
 class CmakeSwitchSchemeCommand(CmakeCommand):
 
     def run(self):
-        ServerManager._servers.pop(self.window.id(), None)x
+        ServerManager._servers.pop(self.window.id(), None)
         self.window.focus_view(self.window.active_view())
 
     @classmethod
